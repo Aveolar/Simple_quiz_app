b92446e67bc4363fe1e299e7bef2c1daa463794a
Adeola Ogunsanya <50545109+Aveolar@users.noreply.github.com>
2020-05-07 14:18:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ul = document.getElementById('ul');
+let ul = document.getElementById('ul');
 const scoreCard = document.querySelector('.scoreCard');
 const totalScore = document.querySelector('.score-card');
 const btn = document.getElementById('next');
@@ -8,28 +8,34 @@ const op2 = document.getElementById('op2');
 const op3 = document.getElementById('op3');
 const op4 = document.getElementById('op4');
 
+
 let app = {
   questions: [
     {
+      pass: false,
       q: 'HTML stands for?',
       options: ['Hyper Text Markup Language', 'High Text Markup Language', 'Hyper Tabular Markup Language', 'None of these'],
       answer: 1
     },
     {
+      pass: false,
       q: 'which of the following tag is used to mark a begining of paragraph ?',
       options: ['td', 'br', 'p', 'tr'],
       answer: 3
     },
     {
+      pass: false,
       q: 'Correct HTML tag for the largest heading is ?', options: ['h4', 'h1', 'h8', 'h9'],
       answer: 2
     },
     {
+      pass: false,
       q: "Who invented JavaScript?",
       options: ['Douglas Crockford', 'Sheryl Sandberg', 'Sheryl Sandberg', 'Brendan Eich'],
       answer: 4
     },
     {
+      pass: false,
       q: "Which one of these is a JavaScript package manager?",
       options: ['Node.js', 'TypeScript', 'npm', 'All of the above'],
       answer: 3
@@ -52,54 +58,54 @@ let app = {
       op2.style.display = "none";
       op3.style.display = "none";
       op4.style.display = "none";
-      btn.style.display = "none";
-      this.scoreCard();
+      btn.innerText = 'Try again';
+      btn.addEventListener("click", () => {
+        location.reload();
+      })
     }
   },
-  next: function () {
-    this.index++;
-    this.load();
-  },
-  check: function (ele) {
-    let id = ele.id.split('');
-
+  check: function (e) {
+    
+    let id = e.id.split('');
+    this.questions[this.index].pass = true;
     if (id[id.length - 1] == this.questions[this.index].answer) {
       this.score++;
-      ele.style.backgroundColor = 'green';
-      ele.style.color = '#fff';
+      e.className = "success";
       this.scoreCard();
     }
     else {
-      ele.style.backgroundColor = 'red';
-      ele.style.color = '#fff';
+      e.className = "error";
+      document.getElementById(`op${this.questions[this.index].answer}`).className = "success";
     }
-    
+  },
+  next: function () {
+    if (this.questions[this.index].pass !== true) {
+      return alert("Please select an option");
+    }
+    this.index++;
+    this.load();
   },
   notClickAble: function () {
     for (let i = 0; i < ul.children.length; i++) {
-      ul.children[i].style.pointerEvents = "none";
+      ul.children[i].classList.add('disabled');
     }
   },
 
   clickAble: function () {
     for (let i = 0; i < ul.children.length; i++) {
-      ul.children[i].style.pointerEvents = "auto";
-      ul.children[i].style.color = '#000';
-      ul.children[i].style.backgroundColor = '';
-
+      ul.children[i].classList.remove('disabled', 'error', 'success');
     }
   },
   score: 0,
   scoreCard: function () {
     scoreCard.innerText = this.score + "/" + this.questions.length;
   }
-  
 }
 
 window.onload = app.load();
 
-function button(ele) {
-  app.check(ele);
+function button(e) {
+  app.check(e);
   app.notClickAble();
 }
 
